5314fca66bb62de31b04f3bdb4ac39239425de52
nhathuych <nhathuych@gmail.com>
2025-11-21 20:53:28 +0700

    add fetching products

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,18 +1,26 @@
-import ProductList from "@/components/ProductList";
-import Image from "next/image";
+import ProductList from '@/components/ProductList';
+import Image from 'next/image';
 
 const Homepage = async ({
   searchParams,
 }: {
-  searchParams: Promise<{ category: string }>;
+  searchParams: Promise<{ category: string, sort: string, search: string }>;
 }) => {
   const category = (await searchParams).category;
+  const search = (await searchParams).search;
+  const sort = (await searchParams).sort;
+
   return (
-    <div className="">
-      <div className="relative aspect-[3/1] mb-12">
-        <Image src="/featured.png" alt="Featured Product" fill />
+    <div className=''>
+      <div className='relative aspect-[3/1] mb-12'>
+        <Image src='/featured.png' alt='Featured Product' fill />
       </div>
-      <ProductList category={category} params="homepage"/>
+      <ProductList
+        category={category}
+        sort={sort}
+        search={search}
+        params='homepage'
+      />
     </div>
   );
 };
